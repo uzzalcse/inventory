cdeb13c85b4092efcecd9425d66f4c9d0fd524cd
uzzalcse <uzzal.cse42@gmail.com>
2024-12-03 15:10:26 +0600

    sign up and login implemented

--- property_management/views.py
+++ property_management/views.py
@@ -1,37 +1,10 @@
-# from django.shortcuts import render
-
-# # Create your views here.
-# from django.shortcuts import render, redirect
-# from django.db import IntegrityError
-# from .forms import PropertyOwnerSignUpForm
-
-# def property_owner_signup(request):
-#     if request.method == 'POST':
-#         form = PropertyOwnerSignUpForm(request.POST)
-#         if form.is_valid():
-#             try:
-#                 form.save()  # Try saving the form data to the database
-#                 return redirect('success')  # Redirect after successful form submission
-#             except IntegrityError as e:
-#                 if 'duplicate key value violates unique constraint' in str(e):
-#                     form.add_error('username', 'This username is already taken. Please choose a different one.')
-#     else:
-#         form = PropertyOwnerSignUpForm()
-
-#     return render(request, 'signup.html', {'form': form})
-
-
-# def success_view(request):
-#     return render(request, 'success.html')
-
-
-
 
 from django.shortcuts import render, redirect
 from django.db import IntegrityError
 from django.core.exceptions import ValidationError
 from .forms import PropertyOwnerSignUpForm
 from .models import User  # Make sure to import the User model
+from django.contrib.auth import logout
 
 def property_owner_signup(request):
     if request.method == 'POST':
@@ -58,4 +31,5 @@ def property_owner_signup(request):
 
 
 def success_view(request):
+    logout(request)
     return render(request, 'success.html')
